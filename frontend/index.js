4bc7cc1bc55c19eabbfa69f51662f7f202ce28aa
kertenkerem <keremdanis@gmail.com>
2026-01-15 02:39:32 +0300

    feat(auth): implement login UI

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Frontend!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Frontend app listening at http://localhost:${port}`);
